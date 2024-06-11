4db558d7c9abae7faf4f6fefe4bf305a32926b5a
CyberKarthi08 <karthikeyan9865602@gmail.com>
2024-06-11 11:56:13 +0530

    Initial commit

---
+++ demo.py
@@ -0,0 +1,28 @@
+
+import threading as th
+import time
+from subprocess import Popen, PIPE
+
+p = Popen(["bc", "-i"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+
+while p.poll() is None:
+    print(p.stdout.readline().decode())
+
+def read_Target(p):
+    while p.poll() is None:
+        print(p.stdout.readline())
+
+t=th.Thread(target=read_Target, args=(p,))
+t.start()
+
+while p.poll() is None:
+    quary = str(input())
+    if quary == 'quit':
+        print(quary)
+        p.communicate(quary,timeout=1)
+
+    if p.poll() is not None:
+        break
+
+p.stdin.write(quary.encode().strip())
+p.stdin.flush()
