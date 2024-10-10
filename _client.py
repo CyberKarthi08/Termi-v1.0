4157b86d8859c8d8c80dc361e3edaf39cebb1071
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-10-10 08:11:47 +0530

    Initial commit

--- _client.py
+++ _client.py
@@ -1,21 +1,71 @@
+import re
 import socket
+from time import sleep
 
-HOST = '127.0.0.1'
-PORT = 5443
+SERVER_IPADDR = '192.168.96.254'
+PORT = 5449
 
-name = input("Enter your name: ");
+__LIST__ = ["Configure your hostname", "Yes | No" ]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-					#AF - Address Family
-					#INET - Internet
+CLIENT_DATA = []
+
+class Client_Connection_Process:
+	def __init__(self,client_sock=None, client_send_message=None,client_recv_msessage=None):
+		self.client_sock = client_sock
+		self.client_send_message = client_send_message
+		self.client_recv_message = client_recv_msessage
+
+	def client_connecting_process(self):
+		self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #AF - Address Family , #INET - Internet
+		self.client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)		
+		self.client_sock.connect((SERVER_IPADDR,PORT))
+		print(f"\n[+] Connection Established By {SERVER_IPADDR}:{PORT}\n")
+		# while True:
+		with self.client_sock:
+			self.client_recv_message = [self.client_sock.recv(1024).decode('utf-8')]
+			initial_message = self.client_recv_message[0].splitlines()
+			for message in initial_message:
+				sleep(1.5)
+				print(f"[+] {message}\n")
+				# if initial_message is True:
+				# 	Client_Connection_Process.send_message()
+			self.client_send_message = input("[*] HOST_Name  @_")
+			self.client_sock.send(self.client_send_message.encode(encoding='UTF-8',errors='strict'))
+			# self.client_recv_message = self.client_sock.recv(1024).decode('utf-8')
+			# print(f"[+] {self.client_recv_message}")
+			Client_Connection_Process.rec_message(self)
+			try:
+				self.client_send_message = input("\n[Yes/No]")
+				self.client_sock.send(self.client_send_message.encode('utf-8')) 
+				
+				self.client_recv_message = self.client_sock.recv(1024).decode('utf-8')
+				print(f"\n{self.client_recv_message}",end="")
+				self.client_send_message = input()
+				self.client_sock.send(self.client_send_message.encode('utf-8'))
+				Client_Connection_Process.rec_message(self)
 					
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect((HOST, PORT))
-print(s.recv(2048).decode())
-while(True):
-	msg = input("Message: ")
-	if(msg == "quit"):
-		break
-	msg = name + ": " + msg
-	s.sendall(msg.encode())
-s.close()
+			except self.client_sock.timeout:
+				print("Error")
+				self.client_recv_message = self.client_recv_message[0].splitlines()
+				print(f"\n[+] {self.client_recv_message[0]}")
+				if(self.client_send_message == "quit"):
+					self.client_sock.close
+
+	def send_message(self):
+			self.client_send_message = input()
+			if(self.client_send_message == "quit"):
+				self.client_sock.close
+			# message = name + ": " + msg
+			self.client_sock.sendall(self.client_send_message.encode('utf-8'))
+
+	def rec_message(self):
+		self.client_recv_message = self.client_sock.recv(1024).decode('utf-8')
+		print(f"\n[+] {self.client_recv_message}")
+
+
+if __name__ == "__main__":
+
+	main_client = Client_Connection_Process()
+	main_client.client_connecting_process()
+	# main_client.send_recv_message_manage()		
+
--- test/.__server__.py
+++ test/.__server__.py
@@ -1,90 +1,118 @@
 import socket
 import threading
-import time
+import subprocess
+from time import sleep
+import codecs
 import os
+import sys
 import ipaddress
 import math
 
-SERVER_IPADDR = "192.168.58.254"
+SERVER_IPADDR = "192.168.96.254"
 PORT = 5449
-MEMBER_DETAILS = {}
+MEMBER_DETAILS = []
 
-class Server_Connection_Process:
+class Server_Connection_Process(threading.Thread):
 
     # If you have store a user's names
    
 
-    def __init__(self, server_sock=None):
-            if server_sock is True:
+    def __init__(self, server_sock=None,client_send_data=None,server_recv_data=None,client_connection_data=None,send_message=None):
                 self.server_sock = server_sock 
+                self.send_message = send_message
+                self.client_send_data = client_send_data
+                self.server_recv_data = server_recv_data
+                self.client_connection_data = client_connection_data
 
-    def server_connection_binding(self):
-        self.server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server_sock.bind((SERVER_IPADDR, PORT)) 
-    def server_listening_process(self):
-        self.server_sock.listen(0)
-        print(f"Listening on {SERVER_IPADDR}:{PORT}")
-        client_recv_data, client_connection_data = self.server_sock.accept()
-        with client_recv_data:
-            print(f"Connection Established By {client_connection_data[0]}:{client_connection_data[1]}")
-            while True:
-                recv_data = client_recv_data.recv(1024)
-                self.server_sock.sendall("[+] Configure your hostname : ")
-                recv_data = recv_data.decode('utf-8')
-                if recv_data == "close":
-                     client_recv_data.send("closed".encode("utf-8"))
-                     break
-                print(f"Data: {recv_data}")
-                client_recv_data.sendall(recv_data)
+    def server_binding_process(self):
+            self.server_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.server_sock.bind((SERVER_IPADDR, PORT))
+
+    def server_connecting_process(self):
+        with self.server_sock:
+            self.server_sock.listen(0)
+            print(f"[+] Listening on {SERVER_IPADDR}:{PORT}")
+            while self.server_sock.listen:
+                self.client_send_data, self.client_connection_data = self.server_sock.accept()
+                print("[+]",self.client_send_data)
+                print("[-]",self.client_connection_data)
+                Server_Connection_Process.Initial_send_messages(self)
+                Server_Connection_Process.server_recv_meassage(self,recv_data=self.client_send_data)
+                # self.server_recv_data = self.client_send_data.recv(1024).decode('utf-8')
+                # MEMBER_DETAILS.append(self.server_recv_data)
+                print(f"[+] Connection Established By @_{MEMBER_DETAILS[0]} {self.client_connection_data[0]}:{self.client_connection_data[1]}")
+
+                Server_Connection_Process.server_send_data(self,send_message=f"@{MEMBER_DETAILS[0]}, Would you like to access our server infrastructure?")
+                Server_Connection_Process.server_recv_meassage(self,recv_data=self.client_send_data)
+                print(MEMBER_DETAILS)
+                if MEMBER_DETAILS[1] == "Yes":
+                    while True:
+                        Server_Connection_Process.server_access_permission(self)
+                        # if MEMBER_DETAILS[2] is True:
+                        #     recv_data = self.client_send_data.recv(1024).decode('utf-8')
+                        #     MEMBER_DETAILS.append(recv_data)
+                        #     print(MEMBER_DETAILS[3])
+                    # task_thread = threading.Thread(target=self.server_access_permission(),args=(self,))
+                    # task_thread.daemon = False # This ensures the thread will exit when the main program exits
+                    # task_thread.start() 
+                else:
+                    request_acess_permission = f"{self.server_recv_data}, If your Connection lost".encode(encoding='UTF-8',errors='strict')
+                    self.client_send_data.send(request_acess_permission)  
+                
+                
+    def server_access_permission(self):
+        while True:
+            print(MEMBER_DETAILS[1])
+            Server_Connection_Process.server_send_data(self,send_message=f"Termi_Server@{MEMBER_DETAILS[0]}:~$ ")
+            self.server_recv_data = self.client_send_data.recv(1024).decode('utf-8')
+            # MEMBER_DETAILS.append(self.server_recv_data) 
+            # Server_Connection_Process.server_recv_meassage(self,recv_data=self.client_send_data)
+                    # MEMBER_DETAILS= self.server_recv_data
+            # print("[+]",MEMBER_DETAILS[2])
+            try:
+                termi_command_result= subprocess.run(self.server_recv_data, shell=True, capture_output=True, text=True)
+                print(termi_command_result)
+                print(type(termi_command_result))
+                output = termi_command_result.stdout + termi_command_result.stderr  # Capture both stdout and stderr
+            except Exception as e:
+                output = str(e)
+
+                        # Send the command output back to the client
+            self.client_send_data.send(output.encode('utf-8'))
+                    # promt_result = [os.system(MEMBER_DETAILS[2])]
+                    # print(type(promt_result))
+                    # print(promt_result)
+                    # promt_result = codecs.encode(promt_result[0], 'utf-8')
+                    # self.client_send_data.send(promt_result)
+                    # return promt_result
+    
+            
+
+    def Initial_send_messages(self):
+        __Initial_message = ["Hey...Hi...👋\nWe welcome you to check out our service 🎉...\nConfigure your hostname\n"]
+        encoded_messages = [message.encode('utf-8') for message in __Initial_message]
+        for encoded_message in encoded_messages:
+            self.client_send_data.send(encoded_message)
+
+    def server_send_data(self,send_message):
+        send_message = codecs.encode(send_message, 'utf-8')
+        self.client_send_data.send(send_message)
+
+    def server_recv_meassage(self,recv_data):
+        recv_data = self.client_send_data.recv(1024).decode('utf-8')
+        MEMBER_DETAILS.append(recv_data)
+        
+         
+          
+
+         
+         
+        
                     
            
 
 if __name__ == "__main__":
     main_thread = Server_Connection_Process()
-    main_thread.server_connection_binding()
-    main_thread.server_listening_process()
-
-    
-            
-
-
-# def termi_interface(self=True):
-            
-
-
-
-#         self.user_name = str(input("Who are you : "))
-#         print(f"Hey, Hi {self.user_name} Welcome to visit our service", end="!!!")
-#         main_thread = threading.Thread(target=main_Termin.termi_interface, args=(self,),daemon=False)
-
-#         print("\nIf you access our Termi-Infrastructure\n")
-#         terminal_access_input = input(" yes  ||  no : \n\n")
-#         if terminal_access_input == 'yes':
-#             print("Wait 2 sec to connecting\n")
-#             time.sleep(2)
-#             while True:
-#                 command = input("$ ")
-#                 os.system(command)
-#                 if command == 'exit':
-#                     print(f"{user} connection quited\n")
-
-#                     break
-#         elif terminal_access_input == 'no':
-#             print("Okay...Let Move Forward ")
-#         else:
-#             print("Error : Type valid input ")
-
-#         main_thread.start()
-#         def get_user_name(self):
-#             print(user_name)
-
-# # demo = main_Termin()
-# main_Termin.termi_interface()
-
-
-
-
-
-
-
-
+    main_thread.server_binding_process()
+    main_thread.server_connecting_process()
