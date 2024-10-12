c03b393219977d5e7a49c948557e1abdafcdec2b
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-10-12 05:55:12 +0530

    Initial commit

--- test/__demo__.py
+++ test/__demo__.py
@@ -1,7 +1,7 @@
 import pyfiglet
 
 def display_project_title():
-    ascii_art = pyfiglet.figlet_format("TREMI", font="slant")
+    ascii_art = pyfiglet.figlet_format("\tARP\nSNIFFER", font="slant")
     print(ascii_art, end="v1.1")
 
 if __name__ == "__main__":
